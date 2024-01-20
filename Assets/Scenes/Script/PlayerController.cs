9986d8d6bb606bb3a58e19cd13742b45a2554ab7
ArchanaSharikalHarinarayanan <93023690+ArchanaSharikalHarinarayanan@users.noreply.github.com>
2024-01-20 14:56:07 +0530

    Update PlayerController.cs

--- Assets/Scenes/Script/PlayerController.cs
+++ Assets/Scenes/Script/PlayerController.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float xForce = 5.0f;
-    public float yForce = 5.0f;
-    public float zForce = 200.0f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -16,29 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = 0.0f;
-       if(Input.GetKey(KeyCode.A))
-        {
-        x = x -xForce;
-        }
-      if(Input.GetKey(KeyCode.D))
-      {
-       x = x + xForce;
-      }
-    float z = 0.0f;
-    if(Input.GetKey(KeyCode.S))
-     {
-    z = z - zForce;
-    }
-    if(Input.GetKey(KeyCode.W))
-    {
-    z = z + zForce;
-    }
-    float y = 0.0f;
-    if(Input.GetKeyDown(KeyCode.Space))
-    {
-    y = yForce;
-    }
-   GetComponent<Rigidbody>().AddForce(x, y, z);
+        // type the code here
     }
 }
